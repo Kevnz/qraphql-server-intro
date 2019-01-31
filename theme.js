2d0dca6e84ab884a925ef999041401298ff585ff
Kevin Isom <kevin.isom@gmail.com>
2019-02-01 11:12:50 +1300

    All The Things

--- theme.js
+++ theme.js
@@ -1,12 +1,29 @@
 import theme from 'mdx-deck/themes'
 
+const yellow = '#ffd500'
+
 export default {
   ...theme,
-
-  // Customize your presentation theme here.
-  //
-  // Read the docs for more info:
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
-  // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
-
+  font: '"Roboto Condensed", system-ui, sans-serif',
+  weights: [400, 700],
+  monospace: '"Roboto Mono", monospace',
+  colors: {
+    text: '#000',
+    background: yellow,
+    link: '#333',
+    pre: yellow,
+    preBackground: '#000',
+    code: yellow,
+    codeBackground: '#000'
+  },
+  heading: {
+    textTransform: 'uppercase',
+    fontWeight: 700
+  },
+  blockquote: {
+    fontWeight: 700
+  },
+  pre: {
+    textAlign: 'left'
+  }
 }
